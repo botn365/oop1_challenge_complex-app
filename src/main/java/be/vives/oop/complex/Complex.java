c93e02d927409c9195da0788c75c7a325c242b8e
korneel vandamme <vandamme.korneel@gmail.com>
2018-12-06 10:59:36 +0100

    add basics for complex

--- src/main/java/be/vives/oop/complex/Complex.java
+++ src/main/java/be/vives/oop/complex/Complex.java
@@ -4,4 +4,33 @@ package be.vives.oop.complex;
  * Complex
  */
 public class Complex {
+    public Complex(){
+        this(0,0);
+    }
+    public Complex(double real,double imaginair) {
+        this.setReal(real);
+        this.setImaginary(imaginair);
+    }
+    public Complex(Complex original){
+        this(original.getRael(),original.getImaginary());
+    }
+    public void setReal(double real){
+        this.real=real;
+    }
+    public void setImaginary(double imaginair){
+        this.imaginair=imaginair;
+    }
+    public double getRael(){
+        return real;
+    }
+    public double getImaginary(){
+        return imaginair;
+    }
+
+
+    //privare
+    private double real=0;
+    private double imaginair=0;
+
+    
 }
